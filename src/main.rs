6b816823b910ce54faf6160340c7871010518f79
esbrinachain <ebrinachain@email.com>
2026-01-16 12:36:48 +0100

    c1

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"ue").unwrap();
+    let text = "my movie queue meue 6";
+
+    match re.find(text) {
+        Some(data) => {
+            println!("{data:?}");
+        }
+        None => println!("No matches found"),
+    }
+}
